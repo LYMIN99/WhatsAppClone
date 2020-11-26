cc9aa30455234c2535374e9068bcba0f657bb576
lymin99 <lymin007@gmail.com>
2020-11-26 09:09:25 +0700

    latest

---
+++ app/src/main/java/com/littleit/whatsappclone/view/activities/status/AddStatusPicActivity.java
@@ -0,0 +1,14 @@
+package com.littleit.whatsappclone.view.activities.status;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddStatusPicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_status_pic);
+    }
+}
